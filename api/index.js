7fd3ff53a61497fd904aad7a7b2bf29fb8bc233e
Enrique Goyret <egoyret@gmail.com>
2021-07-29 09:30:18 -0300

    29-jul-21 PI

--- api/index.js
+++ api/index.js
@@ -27,13 +27,13 @@ const { conn, Diet } = require('./src/db.js');
 // Syncing all the models at once.
 // Inicializo el modelo de base de datos, haciendo los cambios que correspondan en la tablas que ya existan o creando tablas nuevas (force: true).
 // Luego me pongo a escuchar el puerto 3001
-const update = false ;
+const update = true ;
 conn.sync({ force: update }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     if (update)
     {
-      const dietas = ["Gluten Free","Ketogenic", "Vegetarian", "Lacto-Vegetarina", "Ovo-Vegetarian","Vegan","Pescatarian","Paleo","Primal","Whole30", "dairy free"];
+      const dietas = ["Gluten Free","Ketogenic", "Vegetarian", "Lacto-Vegetarina", "lacto ovo vegetarian", "Ovo-Vegetarian","Vegan","Pescatarian","Paleo","Primal","Whole30", "dairy free"];
       dietas.forEach(async (element) => await Diet.create({nombre: element}));
       console.log('Tipos de dieta pre-cargadas')
     }
